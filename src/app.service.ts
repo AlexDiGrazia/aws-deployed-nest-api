4e4df1185c3995848a1674e8920854d0adf4781f
Alex Digrazia <alexndigrazia@hotmail.com>
2024-07-12 19:14:56 -0700

    two separate steps for waiting for each instance to execute.  Also, corrected -rm to --rm

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello world! This week's deployment includes a Postgres Database!";
+    return "Hello world! This week's deployment includes a Postgres Database! Yay!";
   }
 }
