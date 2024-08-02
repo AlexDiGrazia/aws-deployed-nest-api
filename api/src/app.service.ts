29b27503e90fe6d975a5aeb213c24193bc5de38c
Alex Digrazia <alexndigrazia@hotmail.com>
2024-08-01 22:36:24 -0700

    new message on the index endpoint

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello world! woo hoo';
+    return 'Hello world! this text is going to be updated with github actions! yay';
   }
 }
