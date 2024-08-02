11d15e3a0e98fff87ed980f1c9fabe9e1106fda6
Alex Digrazia <alexndigrazia@hotmail.com>
2024-08-01 22:22:56 -0700

    pushing new message to github

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello world! For week 5 we Deployed our API with GitHub Actions Automation!';
+    return 'Hello world! woo hoo';
   }
 }
